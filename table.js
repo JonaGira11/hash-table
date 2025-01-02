290eda7fd614df3a16862d1d79616d435f96695d
Jonathan Giraldo Mosquera <gitboi56@outlook.com>
2025-01-02 15:14:10 +0000

    string hash

--- table.js
+++ table.js
@@ -1,1 +1,11 @@
-// hash table
+// hash table for strings onlyl
+hash("")
+
+function hash(key, arrayLength){
+    let total = 0;
+    for(let char of key) {
+        let value = char.charCodeAt(0)-96
+        total = (total + value) % arrayLength;
+    }
+    return total;
+}
